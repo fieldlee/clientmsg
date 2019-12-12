82ba220db056a5845f3996f044aad28326637a02
fieldlee <ldpfieldlee@gmail.com>
2019-12-12 10:25:50 +0800

    remove handle package and fix circle improt

--- clientmsg.go
+++ clientmsg.go
@@ -5,13 +5,13 @@ import "C"
 
 import (
 	"clientmsg/call"
-	"clientmsg/handle"
 	pb "clientmsg/proto"
 	"clientmsg/utils"
 	"fmt"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"log"
+	"context"
 	"net"
 )
 
@@ -19,6 +19,62 @@ var (
 	Host = utils.Address
 	Port = fmt.Sprintf("%d",utils.Port)
 )
+
+type MsgHandle struct {}
+
+//type HandleMidMsg struct {
+//	Handle func([]byte)([]byte,error)
+//	AsyncHandle func(*pb.SingleResultInfo)([]byte,error)
+//}
+//
+//var HandleObj = HandleMidMsg{}
+
+func (m *MsgHandle)Call(ctx context.Context, info *pb.CallReqInfo) (*pb.CallRspInfo, error) {
+	out := pb.CallRspInfo{}
+
+	/////调用C函数
+
+	//if HandleObj.Handle == nil {
+	//	out.M_Net_Rsp = []byte("The Handle Call function not instance")
+	//}else{
+	//
+	//	//info.Service
+	//	//info.M_Body.M_MsgBody.MLBack
+	//	//info.M_Body.M_MsgBody.MSSendCount
+	//	//info.M_Body.M_MsgBody.MLServerSequence
+	//	//info.M_Body.M_MsgBody.MLExpireTime
+	//	//info.M_Body.M_MsgBody.MLAskSequence
+	//	//info.M_Body.M_MsgBody.MISendTimeApp
+	//	//info.M_Body.M_MsgBody.MCMsgType
+	//	//info.M_Body.M_MsgBody.MCMsgAckType
+	//	//info.M_Body.M_MsgBody.MIDiscard
+	//	//info.M_Body.M_MsgBody.MLAsktype
+	//	//info.M_Body.M_MsgBody.MLResult
+	//
+	//	reT,err := HandleObj.Handle(info.M_Body.M_Msg)
+	//	if err != nil {
+	//		out.M_Net_Rsp = []byte(err.Error())
+	//	}
+	//	out.M_Net_Rsp = reT
+	//}
+	return &out,nil
+}
+
+func (m *MsgHandle)AsyncCall(ctx context.Context, resultInfo *pb.SingleResultInfo) (*pb.CallRspInfo, error) {
+	out := pb.CallRspInfo{}
+	// 调用C函数
+
+	//if HandleObj.AsyncHandle == nil {
+	//	out.M_Net_Rsp = []byte("The AsyncHandle Call function not instance")
+	//}else{
+	//	reT,err := HandleObj.AsyncHandle(resultInfo)
+	//	if err != nil {
+	//		out.M_Net_Rsp = []byte(err.Error())
+	//	}
+	//	out.M_Net_Rsp = reT
+	//}
+	return &out,nil
+}
 
 //export Run
 func Run()  {
@@ -29,7 +85,7 @@ func Run()  {
 		log.Println("server is listening at: " + Host + ":" + Port)
 	}
 	rpcServer := grpc.NewServer()
-	pb.RegisterClientServiceServer(rpcServer,&handle.MsgHandle{})
+	pb.RegisterClientServiceServer(rpcServer,&MsgHandle{})
 	reflection.Register(rpcServer)
 	if err = rpcServer.Serve(listener); err != nil {
 		log.Fatalln("failed serve at: " + Host + ":" + Port)
@@ -53,6 +109,7 @@ func Subscribe(service,ip string){
 
 //export Sync
 func Sync(body []byte){
+	//调用C函数
 	syncResult,err := call.CallSync(body)
 	if err != nil {
 
@@ -62,8 +119,12 @@ func Sync(body []byte){
 
 //export Async
 func Async(body []byte){
+	//调用C函数
 	syncResult,err := call.CallAsync(body)
 	if err != nil {
 
 	}
 }
+
+
+
